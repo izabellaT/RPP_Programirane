75c6163425a6582dd120b2d1e34f163b2fb5001f
izabellaT <izabelatodorova84@gmail.com>
2023-04-19 09:11:49 +0300

    string builder

---
+++ StringBuilderRpp/Zad3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+
+namespace Zad3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder result = new StringBuilder(100);
+            result.Append(text);
+
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split();
+                if (input[0] == "Exit")
+                {
+                    break;
+                }
+
+                switch (input[0])
+                {
+                    case "Аppend":
+                        {
+                            result.Append(input[1]);
+                            break;
+                        }
+
+                    case "Remove":
+                        {
+                            int index = int.Parse(input[1]);
+                            int number = int.Parse(input[2]);
+                            result.Remove(index, number);
+                            break;
+                        }
+
+                    case "Insert":
+                        {
+                            int index = int.Parse(input[1]);
+                            string element = input[2];
+                            result.Insert(index, element);
+                            break;
+                        }
+
+                    case "Replace":
+                        {
+                            string element = input[1];
+                            string element1 = input[2];
+                            result.Replace(element, element1);
+                            break;
+                        }
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine(result.ToString());
+            Console.WriteLine("Finish!");
+        }
+    }
+}
